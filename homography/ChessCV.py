potentiall fixed board rotation bug

homography/ChessCV.py
@@ -10,6 +10,7 @@ class CCV:
                  draw_info=False,
                  cam_mat='../camera_calibration/camera_matrix.csv', dist_coeff='../camera_calibration/dist_coeff.csv'):
         # read in camera matrix and distortion coefficients
+        self.got_video = None
         cam_mat_file = open(cam_mat, 'rb')
         dist_coeff_file = open(dist_coeff, 'rb')
         # read matrices from file
@@ -332,15 +333,16 @@ def order_points(corners, aruco_location):
     corners_reshape = corners.reshape((49, 2))  # reshape to 40x2 array
     corner_dist = [np.sqrt((corner[0] - aruco_center[0]) ** 2 + (corner[1] - aruco_center[1]) ** 2) for corner in
                    corners_reshape]
-    closest_val = closest([0, 6, 42, 48], np.argmin(corner_dist))
+    # closest_val = closest([0, 6, 42, 48], np.argmin(corner_dist))
+    closest_val = closest([0, 1, 7, 5, 6, 13, 35, 42, 43, 41, 47, 48], np.argmin(corner_dist))
 
-    if closest_val == 6:
+    if closest_val in [5, 6, 13]:
         corners_reshape = corners.reshape((7, 7, 2))
         corners_reshape = np.rot90(corners_reshape, 1, axes=(0, 1))
-    elif closest_val == 42:
+    elif closest_val in [35, 42, 43]:
         corners_reshape = corners.reshape((7, 7, 2))
         corners_reshape = np.rot90(corners_reshape, 3, axes=(0, 1))
-    elif closest_val == 48:
+    elif closest_val in [41, 47, 48]:
         corners_reshape = corners.reshape((7, 7, 2))
         corners_reshape = np.rot90(corners_reshape, 2, axes=(0, 1))
 